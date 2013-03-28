23fbb8f5e0b96dfb3b12d6a79c73965d8138196f
Joseph Kiok <joseph@kiok.org>
2013-03-28 00:09:47 -0600

    Interim checkin - refactored further so https support can be added in smoothly.

--- recording_http_proxy.js
+++ recording_http_proxy.js
@@ -26,19 +26,20 @@
 //                      DO NOT MODIFY BELOW THESE LINE                        //
 ////////////////////////////////////////////////////////////////////////////////
 
+var WORK_DIRECTORY = __dirname + "/work/";
+
 ////////////////////////////////////////////////////////////////////////////////
 /// LIBRARIES //////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 var cli = require('commander');
 var connect = require('connect');
-var cookies = require('cookies');
 var fs = require("fs");
 var http = require('http');
+var https = require('https');
 var log4js = require('log4js');
 var moment = require('moment');
 var node_fs = require('node-fs');
-var node_static = require('node-static');
 var fs_extra = require('fs.extra');
 var path = require('path');
 var url = require('url');
@@ -74,6 +75,13 @@ logger.setLevel(cli.debug ? 'DEBUG' : 'ERROR');
 
 var timestampFormat = "YYYY-MM-DDTHH-mm-ss-SSS";
 
+/**
+ * getFileSystemUrl
+ * Return the file URL where to save this HTTP asset to.
+ * @param cacheDirectory
+ * @param requestObj
+ * @param remoteIpAddress
+ */
 function getFileSystemUrl(cacheDirectory, requestObj, remoteIpAddress) {
 
 	// Template: /{cache_directory}/{protocol}/{hostname}/{port}/{pathname}
@@ -103,6 +111,61 @@ function getFileSystemUrl(cacheDirectory, requestObj, remoteIpAddress) {
 }
 
 /**
+ * doInternalProxyRequest
+ * Process the HTTP request - saves the response to the disk, and serve it to the client.
+ * @param incomingMessage
+ * @param requestObj
+ * @param response
+ * @param proxyResponse
+ * @param cacheDirectory
+ * @param clientIp
+ */
+function doInternalProxyRequest(incomingMessage, requestObj, response,
+	proxyResponse, cacheDirectory, clientIp) {
+
+	// Step 1: Figure out the file URL to save the asset to
+	var fileUrl = getFileSystemUrl(cacheDirectory, requestObj, clientIp);
+	logger.debug("[" + incomingMessage.url + "] -> " + fileUrl);
+
+	// Should we save this HTTP asset?
+	var saveToDisk = true;
+
+	var directory = path.dirname(fileUrl);
+	if (!fs.existsSync(directory)) {
+		logger.debug("Creating directory [" + directory + "] for fileUrl [" + fileUrl + "]");
+		node_fs.mkdirSync(path.dirname(fileUrl), 0755, true);
+	} else {
+
+		if (fs.statSync(directory).isFile()) {
+			logger.error("Failed to create directory [" + directory + "] because there is a file with the same name");
+			saveToDisk = false;
+		}
+	}
+
+	var fh = null;
+	if (saveToDisk) {
+		fh = fs.createWriteStream(fileUrl);
+	}
+
+	// Step 2: Write out the HTTP status code and headers
+	response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+
+	// Step 3: Serve the chunk and close connection
+	proxyResponse.on("data", function(chunk) {
+		response.write(chunk, 'binary');
+		if (saveToDisk) fh.write(chunk);
+	}).on("end", function() {
+		response.end();
+		if (saveToDisk) fh.end();
+		if (saveToDisk) fh.destroy();
+	}).on("close", function() {
+		response.end();
+		if (saveToDisk) fh.end();
+		if (saveToDisk) fh.destroy();
+	});
+};
+
+/**
  * doInternalProxy
  * Process the proxied incoming HTTP transaction.
  * @param incomingMessage
@@ -125,54 +188,19 @@ function doInternalProxy(incomingMessage, cacheDirectory, response) {
 
 	var clientIp = incomingMessage.connection.remoteAddress;
 
-	var proxyRequest = http.request(options, function(proxyResponse) {
+	var proxyRequest = null;
 
-		// Step 3: Figure out the file URL to save the asset to
-		var fileUrl = getFileSystemUrl(cacheDirectory, requestObj,
-			clientIp);
-		logger.debug("[" + incomingMessage.url + "] -> " + fileUrl);
+	if (requestObj['protocol'] == 'http:') {
+		proxyRequest = http.request(options, function(proxyResponse) {
+			doInternalProxyRequest(incomingMessage, requestObj, response, proxyResponse, cacheDirectory, clientIp);
+	    });
+	} else if (requestObj['protocol'] == 'https:') {
+		proxyRequest = https.request(options, function(proxyResponse) {
+			doInternalProxyRequest(incomingMessage, requestObj, response, proxyResponse, cacheDirectory, clientIp);
+	    });
+	}
 
-		// Should we save this HTTP asset
-		var saveToDisk = true;
-
-		var directory = path.dirname(fileUrl);
-		if (!fs.existsSync(directory)) {
-
-			logger.debug("Creating directory [" + directory + "] for fileUrl [" + fileUrl + "]");
-			node_fs.mkdirSync(path.dirname(fileUrl), 0755, true);
-
-		} else {
-
-			if (fs.statSync(directory).isFile()) {
-				logger.error("Failed to create directory [" + directory + "] because there is a file with the same name");
-				saveToDisk = false;
-			}
-		}
-
-		var fh = null;
-		if (saveToDisk) {
-			fh = fs.createWriteStream(fileUrl);
-		}
-
-		// Step 4: Write out the HTTP status code and headers
-		response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
-
-		// Step 5: Serve the chunk and close connection
-        proxyResponse.on("data", function(chunk) {
-			response.write(chunk, 'binary');
-            if (saveToDisk) fh.write(chunk);
-        }).on("end", function() {
-			response.end();
-            if (saveToDisk) fh.end();
-            if (saveToDisk) fh.destroy();
-        }).on("close", function() {
-			response.end();
-			if (saveToDisk) fh.end();
-			if (saveToDisk) fh.destroy();
-		});
-    });
-
-	// Step 6: For POST data, be sure to pass it along
+	// Step 3: For POST data, be sure to pass it along
 	incomingMessage.on("data", function(chunk) {
 		proxyRequest.write(chunk);
 	}).on("end", function() {
@@ -192,17 +220,19 @@ process.on('uncaughtException', function(err) {
 });
 
 http.createServer(function(incomingMessage, response) {
-	doInternalProxy(incomingMessage, __dirname + "/work", response);
+	doInternalProxy(incomingMessage, WORK_DIRECTORY, response);
+}).on("connect", function(request, socket, head) {
+	// TODO: doInternalProxy(request, WORK_DIRECTORY, null);
 }).listen(parseInt(cli.proxy_port));
 
 connect()
-	.use(connect.static(__dirname + "/work"))
-	.use(connect.directory(__dirname + "/work"))
+	.use(connect.static(WORK_DIRECTORY))
+	.use(connect.directory(WORK_DIRECTORY))
 	.use(function(req, res) {
 		if (req.url.indexOf('clear') != -1) {
 			var ip = req.connection.remoteAddress;
 			logger.debug("Deleting directory for " + ip);
-			fs_extra.rmrfSync(__dirname + "/work/" + ip);
+			fs_extra.rmrfSync(WORK_DIRECTORY + ip);
 			res.writeHead(200, null);
 			res.end();
 		}
